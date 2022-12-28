62b2f1625fa238c34032d48f1509a2be4d2b8d71
lacu <lacu@gft.com>
2022-12-28 16:04:21 -0300

    Add layout improvements and corrections

--- src/main/java/com/lcrtech/productregistration/config/SecurityConfig.java
+++ src/main/java/com/lcrtech/productregistration/config/SecurityConfig.java
@@ -3,9 +3,24 @@ package com.lcrtech.productregistration.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder builder)
